6f9bf9c24c1290f9fd42e582469857a4785ef61c
RayPhenom <lumuonix@gmail.com>
2023-10-01 18:59:56 +0300

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './home.css'
 import video from '../../Assets/video.mp4'
-import {GrLocation} from 'react-icons/gr'
+import { GrLocation } from 'react-icons/gr'
+import { HiFilter } from 'react-icons/hi'
+
 
 const Home = () => {
-    return(
+    return (
         <section className='home'>
             <div className="overlay"></div>
-            <video src={video} muted autoPlay loop type ="video/mp4"></video>
+            <video src={video} muted autoPlay loop type="video/mp4"></video>
 
             <div className="homecontent container">
                 <div className="textDiv">
@@ -20,16 +22,45 @@ const Home = () => {
 
                 </div>
 
+                <div className="cardDiv grid"></div>
+                <div className="destinationInput">
+                    <label htmlFor="city">Search your destination:</label>
+                    <div className="input flex">
+                        <input type="text" placeholder='Enter name here...' />
+                        <GrLocation className="icon" />
+                    </div>
+
+                </div>
+
+
                 <div className="cardDiv grid">
                     <div className="dateInput">
                         <label htmlFor=" date">Select your date:</label>
                         <div className="input flex">
                             <input type="date" />
-                            <GrLocation className="icon"/>
+                            <GrLocation className="icon" />
 
 
                         </div>
                     </div>
+
+                    <div className="priceInput">
+                        <div className="label_total flex">
+                            <label htmlFor="price">Max price:
+                            </label>
+                            <h3 className="total">$10000 </h3>
+
+                        </div>
+                        <div className="input flex">
+                            <input type="range" max="10000" min="500" />
+                        </div>
+                        <div className="searchOptions flex">
+                            <HiFilter className="icon" />
+                        </div>
+
+
+                    </div>
+
                 </div>
             </div>
 
